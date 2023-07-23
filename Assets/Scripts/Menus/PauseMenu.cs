0757c6ff73c8871f24b966d6781cd1044bcfab34
nilPuigdemont <121803376+nilPuigdemont@users.noreply.github.com>
2023-07-23 16:13:46 +0200

    Pause Menu

---
+++ Assets/Scripts/Menus/PauseMenu.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseMenu;
+
+    bool pause = false;
+
+    public void Pause()
+    {
+        Time.timeScale = 0f;
+        pauseMenu.SetActive(true);
+    }
+
+    public void resume()
+    {
+        Time.timeScale = 1f;
+        pauseMenu.SetActive(false);
+    }
+
+    public void Exit()
+    {
+        Debug.Log("Exit");
+        Application.Quit();
+    }
+
+    public void ExitToMenu()
+    {
+        SceneManager.LoadScene("Scenes/MainMenu");
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (!pause)
+            {
+                pause = true;
+                Pause();
+            }
+            else
+            {
+                pause = false;
+                resume();
+            }
+        }
+    }
+}
