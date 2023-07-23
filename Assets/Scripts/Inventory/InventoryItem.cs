4b3671738e502f44db8ade966d7bbaf8a4553bc6
AroaOchoa <8aaroa@gmail.com>
2023-07-23 16:01:58 +0200

    Level 1 + yes

--- Assets/Scripts/Inventory/InventoryItem.cs
+++ Assets/Scripts/Inventory/InventoryItem.cs
@@ -64,4 +64,12 @@ public class InventoryItem : MonoBehaviour
         Debug.Log("GameObject " + _itemData.name + " is being destroyed!"); // DELETE LATER
         Destroy(gameObject);
     }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            Destroy(gameObject);
+        }
+    }
 }
