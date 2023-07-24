b2f733fcc3331868f6d65250a7ac0f12c0676004
nilPuigdemont <121803376+nilPuigdemont@users.noreply.github.com>
2023-07-24 12:44:16 +0200

    Puntuation Stars

---
+++ Assets/Scripts/Score/ShowStars.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShowStars : MonoBehaviour
+{
+    public int OneStarScore;
+    public GameObject Star1;
+
+    public int TwoStarsScore;
+    public GameObject Star2;
+
+    public int ThreeStarsScore;
+    public GameObject Star3;
+
+    private int totalScore;
+
+    private void Start()
+    {
+        totalScore= PlayerPrefs.GetInt("Score");
+        showStars();
+    }
+
+    public void showStars()
+    {
+        if (totalScore > OneStarScore) Star1.SetActive(true);
+        if (totalScore > TwoStarsScore) Star2.SetActive(true);
+        if (totalScore > ThreeStarsScore) Star3.SetActive(true);
+    }
+}
