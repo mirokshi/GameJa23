9effacb58de3d1810a0331d4f20bd8a24693049b
AroaOchoa <8aaroa@gmail.com>
2023-07-24 13:50:12 +0200

    EndLevel

---
+++ Assets/Scripts/Menus/EndLevel.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class EndLevel : MonoBehaviour
+{
+    public string escena;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(escena);
+        }
+    }
+}
