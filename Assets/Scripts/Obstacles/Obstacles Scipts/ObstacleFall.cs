b3e2e8fe2408e6bada0124dc09548a1b6dbb7012
Júlia Ting Castro Sevilla <121794466+MoonCrow03@users.noreply.github.com>
2023-07-24 12:26:08 +0200

    Obstacles

---
+++ Assets/Scripts/Obstacles/Obstacles Scipts/ObstacleFall.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObstacleFall : ObstacleAction
+{
+    public override void DoAction()
+    {
+        Debug.Log("Success! (Fall)");
+    }
+}
