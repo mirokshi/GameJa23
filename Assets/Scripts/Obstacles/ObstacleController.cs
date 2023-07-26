833efc3a93f99c81bcb7b4542d5008684d323a21
Campalans03 <jancampalans@gmail.com>
2023-07-26 21:45:21 +0200

    fondo ciudad

--- Assets/Scripts/Obstacles/ObstacleController.cs
+++ Assets/Scripts/Obstacles/ObstacleController.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Mathematics;
 using UnityEngine;
 using UnityEngine.Serialization;
 
@@ -24,6 +25,7 @@ public class ObstacleController : MonoBehaviour
         
         else
         {
+
             Destroy(gameObject.GetComponent<BoxCollider2D>());
             _obstacleAction.DoAction(itemData, weight);
         }
